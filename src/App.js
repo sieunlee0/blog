4adc092b3dad625f0398abd4f9202d21ef53265e
sieunlee <dltldmsm@gmail.com>
2021-09-18 13:32:29 +0900

    210918

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = "블로그 글"
+
   return (
     <div className="App">
       <div className="black-nav">
-        <div>개발 Blog</div>
+        <div style={ { color: "blue", fontSize: "30px" } }>
+          개발 Blog</div>
       </div>
+      <img src={ logo }></img>
+      <h3> { posts } </h3>
     </div>
   );
 }
