cc9083e65ee2a6938d713a8b21770f94d7de31f7
sieunlee <dltldmsm@gmail.com>
2021-09-18 13:48:36 +0900

    210918

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState('디저트 맛집 추천');
 
   let posts = "새로운 블로그 글"
 
@@ -11,7 +14,7 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3> { posts } </h3>
+        <h3> { 글제목 } </h3>
         <p>2021년 9월 18일</p>
         <hr/>
       </div>
